ab0f08a1c82ca47ad4cbb7b83529ebcff62fd9a3
Marcos Junior <44973778+silva1996@users.noreply.github.com>
2023-08-03 12:53:44 -0300

    Update login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -23,7 +23,7 @@ describe('Login', () => {
     it('senha incorreta', () => {
         cy.login('qa', 'teste')
         cy.get(toast)
-            .should('have.text', 'Oops Credenciais inválidas :(')
+            .should('have.text', 'Oops! Credenciais inválidas :(')
     })
 
     it('com sucesso', () => {
